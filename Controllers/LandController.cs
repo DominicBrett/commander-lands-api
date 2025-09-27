20bdfa09a2cff404c2ff4b0ddb1d2e5e70c2a43e
DominicBrett <dombrett777@outlook.com>
2025-09-27 16:03:26 +0100

    initial commit

---
+++ Controllers/LandController.cs
@@ -0,0 +1,17 @@
+﻿using Application;
+using Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace commander_lands_api.Controllers
+{
+    [ApiController]
+    [Route("/Lands/")]
+    public class LandController(ILandOrchestrator landOrchestrator)
+    {
+        [HttpGet]
+        public List<LandTypeCollection> GroupedByPriority([FromQuery] string[] colorsProduced)
+        {
+            return landOrchestrator.GetLandTypeCollections(colorsProduced);
+        } 
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+using Application;
+using Domain;
+using Domain.Interfaces;
+using Infrastructure.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<ILandOrchestrator, LandOrchestrator>();
+builder.Services.AddSingleton<ILandDomain, LandDomain>();
+builder.Services.AddSingleton<ILandRepository, LandRepository>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
